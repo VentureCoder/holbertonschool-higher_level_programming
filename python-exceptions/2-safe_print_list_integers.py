b583e99e8bc4885704a6f4c6d7c8b08add4dc806
Orkhan Suleymanov <osul0003@student.monash.edu>
2025-12-06 20:46:29 +0000

    Fix 2-safe_print_list_integers to let IndexError propagate

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,10 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
+        elem = my_list[i]
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(elem), end="")
             count += 1
-        except Exception:
+        except (ValueError, TypeError):
             continue
     print()
     return count
