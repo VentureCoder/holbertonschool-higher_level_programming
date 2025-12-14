dc62669d827583fe2983174eeaaf02e7050a1ce7
Orkhan Suleymanov <osul0003@student.monash.edu>
2025-12-14 20:24:59 +0000

    Add CustomObject pickle serialization

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Custom object serialization using pickle."""
+
+import pickle
+
+class CustomObject:
+    """Custom object class."""
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Display object attributes."""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """Serialize object to a file using pickle."""
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except Exception:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize object from a pickle file."""
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except Exception:
+            return None
