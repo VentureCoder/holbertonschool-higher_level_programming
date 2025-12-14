cc89f7dfdae44011f9f4693b6d7ad5f5cb3cf437
Orkhan Suleymanov <osul0003@student.monash.edu>
2025-12-14 19:44:44 +0000

    Add missing-arg tests and support integer_validator with default value

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -9,9 +9,9 @@ class BaseGeometry:
         """Raise an exception for area."""
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
+    def integer_validator(self, name, value=None):
         """Validate that value is a positive integer."""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
